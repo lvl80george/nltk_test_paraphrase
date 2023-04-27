44d24ceb04704dbd641a23ded2009bf74c7c194e
george_u <heorhii.uvarov@nure.ua>
2023-04-27 19:49:20 +0300

    Fixed: json output containing \n sequences in string representations of the trees

--- rest_api.py
+++ rest_api.py
@@ -7,16 +7,26 @@ app = Flask(__name__)
 
 @app.route('/paraphrase', methods=['GET'])
 def paraphrase_api():
+    # get the arguments
     tree_str = request.args.get("tree") # obligatory argument, the tree's representation
-    limit_str = request.args.get('limit', 20) # optional argument
-    
+    limit_str = request.args.get('limit', 20) # optional argument, default is 20
+
     tree = Tree.fromstring(tree_str)
     limit = int(limit_str)
-    
-    result = paraphrase(tree,limit)
-    # turn each tree into string representation
-    result_str = [{'tree':str(r)} for r in result]
-    return jsonify({'paraphrases': result_str})
 
-# run it from here in debug
+    # construct the paraphrases
+    result = paraphrase(tree,limit)
+    
+    # turn each tree into a string representation
+    infinity = float('inf')
+    # nltk adds \n when printing big trees to make the
+    # output multiline (I don't want to see them)
+    # converting trees to string with pformat and setting margin maximal
+    # makes it output virtually any tree without \n line separators
+    result_str = [{'tree':r.pformat(margin=infinity)} for r in result]
+    return jsonify({'paraphrases': result_str})
+    # returns json like
+    # {  "paraphrases": [ {"tree":...}, {"tree":....}....
+
+# run it from here in debug mode
 app.run(debug=True)
